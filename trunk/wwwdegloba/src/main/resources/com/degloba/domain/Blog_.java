a55999df69d16fc35473b9f1446606c4604eb11a
degloba@degloba.com <degloba@degloba.com@93bdc6e3-e1b6-81dc-b551-2ef16b264382>
2014-06-26 16:48:12 +0000

    ho genera el "JPA Facet"

---
+++ trunk/wwwdegloba/src/main/resources/com/degloba/domain/Blog_.java
@@ -0,0 +1,14 @@
+package com.degloba.domain;
+
+import com.google.appengine.api.datastore.Key;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2014-06-26T18:41:13.618+0200")
+@StaticMetamodel(Blog.class)
+public class Blog_ {
+	public static volatile SingularAttribute<Blog, Key> id;
+	public static volatile SingularAttribute<Blog, String> titol;
+	public static volatile SingularAttribute<Blog, String> descripcio;
+}
