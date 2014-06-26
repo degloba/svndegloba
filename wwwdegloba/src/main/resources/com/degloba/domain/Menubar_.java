386937b13346a74f1e5ed675cae082bc33e5d562
degloba@degloba.com <degloba@degloba.com@93bdc6e3-e1b6-81dc-b551-2ef16b264382>
2014-06-26 16:48:12 +0000

    ho genera el "JPA Facet"

---
+++ wwwdegloba/src/main/resources/com/degloba/domain/Menubar_.java
@@ -0,0 +1,16 @@
+package com.degloba.domain;
+
+import com.google.appengine.api.datastore.Key;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2014-06-26T18:41:13.637+0200")
+@StaticMetamodel(Menubar.class)
+public class Menubar_ {
+	public static volatile SingularAttribute<Menubar, Key> id;
+	public static volatile SingularAttribute<Menubar, String> abrev1;
+	public static volatile SingularAttribute<Menubar, String> abrev2;
+	public static volatile SingularAttribute<Menubar, String> abrev3;
+	public static volatile SingularAttribute<Menubar, Integer> idmenuitem;
+}
---
+++ wwwdegloba/src/main/resources/com/degloba/domain/TipusFramework_.java
@@ -0,0 +1,15 @@
+package com.degloba.domain;
+
+import com.google.appengine.api.datastore.Key;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2014-06-26T18:41:13.655+0200")
+@StaticMetamodel(TipusFramework.class)
+public class TipusFramework_ {
+	public static volatile SingularAttribute<TipusFramework, Key> id;
+	public static volatile SingularAttribute<TipusFramework, String> nom;
+	public static volatile CollectionAttribute<TipusFramework, Framework> frameworks;
+}
