b3e7261af4df80dc3cbb544c031f622eed61c609
degloba@degloba.com <degloba@degloba.com@93bdc6e3-e1b6-81dc-b551-2ef16b264382>
2014-07-29 17:41:22 +0000

    client

---
+++ trunk/wwwdegloba/src/main/java/com/degloba/boundedContext/infrastructure/repositories/impl/JpaClientRepository.java
@@ -0,0 +1,21 @@
+package com.degloba.boundedContext.infrastructure.repositories.impl;
+
+import com.degloba.boundedContext.domain.Client;
+import com.degloba.boundedContext.domain.ClientRepository;
+
+import infrastructure.repository.jpa.GenericJpaRepository;
+import domain.annotations.DomainRepositoryImpl;
+import domain.canonicalmodel.publishedlanguage.AggregateId;
+
+@DomainRepositoryImpl
+public class JpaClientRepository extends GenericJpaRepository<Client, AggregateId> implements ClientRepository<AggregateId> {
+//public class JpaClientRepository extends GenericJpaRepository<Client, Long> implements ClientRepository<Long> {
+
+	@Override
+	public Client load(AggregateId clientId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
