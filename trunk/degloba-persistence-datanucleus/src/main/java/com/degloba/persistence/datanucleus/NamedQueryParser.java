02d411a6b71659909f7d6d03a65141023719345d
degloba@degloba.com <degloba@degloba.com@93bdc6e3-e1b6-81dc-b551-2ef16b264382>
2015-02-03 18:28:29 +0000

    Projecte Persistencia de deGloba

---
+++ trunk/degloba-persistence-datanucleus/src/main/java/com/degloba/persistence/datanucleus/NamedQueryParser.java
@@ -0,0 +1,36 @@
+package com.degloba.persistence.datanucleus;
+
+import javax.persistence.EntityManager;
+
+/**
+ * ç”¨äºŽèŽ·å�–å‘½å��æŸ¥è¯¢çš„æŸ¥è¯¢å­—ç¬¦ä¸²ã€‚ç”±äºŽJPAè§„èŒƒä¸�ç›´æŽ¥æ”¯æŒ�è¿™ä¸€åŠŸèƒ½ï¼Œæ‰€ä»¥è¦�ç”±ä½¿ç”¨JPAå®žçŽ°çš„æœ¬åœ°API
+ * å®žçŽ°å®ƒã€‚
+ * @author yyang
+ */
+public abstract class NamedQueryParser {
+    
+    private EntityManagerProvider entityManagerProvider;
+
+    public NamedQueryParser() {
+    }
+
+    public NamedQueryParser(EntityManagerProvider entityManagerProvider) {
+        this.entityManagerProvider = entityManagerProvider;
+    }
+
+    public void setEntityManagerProvider(EntityManagerProvider entityManagerProvider) {
+		this.entityManagerProvider = entityManagerProvider;
+	}
+
+	protected EntityManager getEntityManager() {
+        return entityManagerProvider.getEntityManager();
+    }
+    
+    /**
+     * èŽ·å�–å‘½å��æŸ¥è¯¢çš„æŸ¥è¯¢å­—ç¬¦ä¸²
+     * @param queryName å‘½å��æŸ¥è¯¢çš„å��å­—
+     * @return å‘½å��æŸ¥è¯¢çš„æŸ¥è¯¢å­—ç¬¦ä¸²
+     */
+    public abstract String getQueryStringOfNamedQuery(String queryName);
+    
+}
