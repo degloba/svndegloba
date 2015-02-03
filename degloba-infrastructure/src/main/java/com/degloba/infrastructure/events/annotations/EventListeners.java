8cd35df7c84520e619a60709f4f3a77c91ffbaf5
degloba@degloba.com <degloba@degloba.com@93bdc6e3-e1b6-81dc-b551-2ef16b264382>
2015-02-03 18:18:22 +0000

    Projecte Infrastructure de deGloba

---
+++ degloba-infrastructure/src/main/java/com/degloba/infrastructure/events/annotations/EventListeners.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.degloba.infrastructure.events.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Slawek
+ * 
+ */
+@Component
+@Target(ElementType.TYPE)
+public @interface EventListeners {
+
+}
