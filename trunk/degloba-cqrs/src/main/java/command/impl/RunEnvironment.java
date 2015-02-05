410e2e0d24527e1dcec70a8de2ae3f16e48b384d
degloba@degloba.com <degloba@degloba.com@93bdc6e3-e1b6-81dc-b551-2ef16b264382>
2015-02-05 17:03:03 +0000

    Projecte CQRS de deGloba

---
+++ trunk/degloba-cqrs/src/main/java/command/impl/RunEnvironment.java
@@ -0,0 +1,47 @@
+package command.impl;
+
+import javax.inject.Inject;
+
+// SPRING
+import org.springframework.stereotype.Component;
+
+// CQRS
+import command.handler.ICommandHandler;
+
+/**
+ * @author degloba
+ * @param <K>
+ */
+@Component
+public class RunEnvironment {
+
+	public interface IHandlersProvider{
+		ICommandHandler<Object> getHandler(Object command);
+	}
+	
+	@Inject
+	private IHandlersProvider handlersProvider;
+	
+	public Object run(Object command) {		
+		ICommandHandler<Object> handler = handlersProvider.getHandler(command);
+		
+		//You can add Your own capabilities here: dependency injection, security, transaction management, logging, profiling, spying, storing commands, etc
+		
+		Object result = handler.handle(command);
+
+		//You can add Your own capabilities here
+		
+		return result;
+	}
+
+	public IHandlersProvider getHandlersProvider() {
+		return handlersProvider;
+	}
+
+	public void setHandlersProvider(IHandlersProvider handlersProvider) {
+		this.handlersProvider = handlersProvider;
+	}
+
+	
+	
+}
