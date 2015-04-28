4862a8d14702fae0d59bc443c8eb6c2f94b82538
degloba <degloba@degloba.com>
2015-04-28 19:09:38 +0200

    Refactoring

--- degloba-domain/src/main/java/com/degloba/domain/BaseQuery.java
+++ degloba-domain/src/main/java/com/degloba/domain/BaseQuery.java
@@ -18,14 +18,15 @@ package com.degloba.domain;
 
 import java.util.List;
 import java.util.Map;
+
 import org.dayatang.utils.Assert;
 
 /**
- * 查询基类，为NamedQuery、JpqlQuery和SqlQuery提供共同行为。
- * @author yyang
- * @param <E> 查询的类型
+ * Query base class for NamedQuery, JpqlQuery and SqlQuery provide common behavior.
+ * @author degloba
+ * @param <E> Types of queries
  */
-public abstract class BaseQuery<E extends BaseQuery> {
+public abstract class BaseQuery<E extends BaseQuery<?>> {
     private final EntityRepository repository;
     private QueryParameters parameters = PositionalParameters.create();
     private final NamedParameters mapParameters = NamedParameters.create();
@@ -38,135 +39,141 @@ public abstract class BaseQuery<E extends BaseQuery> {
     }
 
     /**
-     * 获取查询参数
-     * @return 查询参数
+     * Get query parameters
+     * @return Query parameters
      */
     public QueryParameters getParameters() {
         return parameters;
     }
 
     /**
-     * 设置定位命名参数（数组方式）
-     * @param parameters 要设置的参数
-     * @return 该对象本身
+     * Set positioning named parameters (array mode)
+     * @param parameters Parameters to be set
+     * @return The object itself
      */
-    public E setParameters(Object... parameters) {
+    @SuppressWarnings("unchecked")
+	public E setParameters(Object... parameters) {
         this.parameters = PositionalParameters.create(parameters);
         return (E)this;
     }
 
     /**
-     * 设置定位参数（列表方式）
-     * @param parameters 要设置的参数
-     * @return 该对象本身
+     * Setting positioning parameters (list mode)
+     * @param parameters Parameters to be set
+     * @return The object itself
      */
-    public E setParameters(List<Object> parameters) {
+    @SuppressWarnings("unchecked")
+	public E setParameters(List<Object> parameters) {
         this.parameters = PositionalParameters.create(parameters);
         return (E) this;
     }
 
     /**
-     * 设置命名参数（Map形式，Key是参数名称，Value是参数值）
-     * @param parameters 要设置的参数
-     * @return 该对象本身
+     * Named parameter set (Map form, Key is the parameter name, Value is the parameter value)
+     * @param parameters Parameters to be set
+     * @return The object itself
      */
-    public E setParameters(Map<String, Object> parameters) {
+    @SuppressWarnings("unchecked")
+	public E setParameters(Map<String, Object> parameters) {
         this.parameters = NamedParameters.create(parameters);
         return (E) this;
     }
 
     /**
-     * 添加一个命名参数，Key是参数名称，Value是参数值。
-     * @param key 命名参数名称
-     * @param value 参数值
-     * @return 该对象本身
+     * Add a named parameter, Key is the parameter name, Value is the parameter value.
+     * @param key Named parameter name
+     * @param value Parameter Value
+     * @return The object itself
      */
-    public E addParameter(String key, Object value) {
+    @SuppressWarnings("unchecked")
+	public E addParameter(String key, Object value) {
         mapParameters.add(key, value);
         this.parameters = mapParameters;
         return (E) this;
     }
 
     /**
-     * 设置命名参数（Map形式，Key是参数名称，Value是参数值）
-     * @param parameters 要设置的参数
-     * @return 该对象本身
+     * Named parameter set (Map form, Key is the parameter name, Value is the parameter value)
+     * @param parameters Parameters to be set
+     * @return The object itself
      */
-    public E setParameters(QueryParameters parameters) {
+    @SuppressWarnings("unchecked")
+	public E setParameters(QueryParameters parameters) {
         this.parameters = parameters;
         return (E) this;
     }
 
     /**
-     * 针对分页查询，获取firstResult。
-     * firstResult代表从满足查询条件的记录的第firstResult + 1条开始获取数据子集。
-     * @return firstResult的设置值，
+     * For paging query, get firstResult.
+     * firstResult Representative start getting data from a subset of records that satisfy the query first firstResult + 1 strips.
+     * @return firstResult Setting value,
      */
     public int getFirstResult() {
         return firstResult;
     }
 
     /**
-     * 针对分页查询，设置firstResult。
-     * firstResult代表从满足查询条件的记录的第firstResult + 1条开始获取数据子集。
-     * @param firstResult 要设置的firstResult值。
-     * @return 该对象本身
+     * For paging query, set firstResult.
+     * firstResult Representative start getting data from a subset of records that satisfy the query first firstResult + 1 strips.
+     * @param firstResult FirstResult to set values.
+     * @return The object itself
      */
-    public E setFirstResult(int firstResult) {
+    @SuppressWarnings("unchecked")
+	public E setFirstResult(int firstResult) {
         Assert.isTrue(firstResult >= 0);
         this.firstResult = firstResult;
         return (E) this;
     }
 
     /**
-     * 针对分页查询，获取maxResults设置值。
-     * maxResults代表从满足查询条件的结果中最多获取的数据记录的数量。
-     * @return maxResults的设置值。
+     * For paging query, get maxResults setting.
+     * maxResultsGet the largest number of representatives from the results that satisfy the query data records.
+     * @return maxResults Settings.
      */
     public int getMaxResults() {
         return maxResults;
     }
 
     /**
-     * 针对分页查询，设置maxResults的值。
-     * maxResults代表从满足查询条件的结果中最多获取的数据记录的数量。
-     * @param maxResults 要设置的maxResults值
-     * @return 该对象本身
+     * For paging query, set the value maxResults of.
+     * maxResults Get the largest number of representatives from the results that satisfy the query data records.
+     * @param maxResults MaxResults value to be set
+     * @return The object itself
      */
-    public E setMaxResults(int maxResults) {
+    @SuppressWarnings("unchecked")
+	public E setMaxResults(int maxResults) {
         Assert.isTrue(maxResults > 0);
         this.maxResults = maxResults;
         return (E) this;
     }
 
     /**
-     * 返回查询结果列表。
-     * @param <T> 查询结果的列表元素类型
-     * @return 查询结果。
+     * Return query results list.
+     * @param <T> Query results list element type
+     * @return Query results.
      */
     public abstract <T> List<T> list();
 
     /**
-     * 返回单条查询结果。
-     * @param <T> 查询结果的类型
-     * @return 查询结果。
+     * Returns a single query results.
+     * @param <T> Type of query results
+     * @return Query results.
      */
     public abstract <T> T singleResult();
 
     /**
-     * 执行更新仓储的操作。
-     * @return 被更新或删除的实体的数量
+     * Warehousing operations to perform the update.
+     * @return Number of updated or deleted entities
      */
     public abstract int executeUpdate();
 
     /**
-     * 获得仓储对象。
-     * @return 仓储对象
+     * Get Warehousing object.
+     * @return Warehousing object
      */
     protected EntityRepository getRepository() {
         return repository;
     }
-    
     
     
 }
