9af2a26aa850bbb608b2baa916660b71250b987e
degloba <degloba@degloba.com>
2015-05-23 19:17:45 +0200

    Refactoring

--- degloba-persistence-datanucleus/src/main/java/com/degloba/persistence/datanucleus/NamedQueryParser.java
+++ degloba-persistence-datanucleus/src/main/java/com/degloba/persistence/datanucleus/NamedQueryParser.java
@@ -1,10 +1,9 @@
 package com.degloba.persistence.datanucleus;
 
+// JPA
 import javax.persistence.EntityManager;
 
 /**
- * ç”¨äºŽèŽ·å�–å‘½å��æŸ¥è¯¢çš„æŸ¥è¯¢å­—ç¬¦ä¸²ã€‚ç”±äºŽJPAè§„èŒƒä¸�ç›´æŽ¥æ”¯æŒ�è¿™ä¸€åŠŸèƒ½ï¼Œæ‰€ä»¥è¦�ç”±ä½¿ç”¨JPAå®žçŽ°çš„æœ¬åœ°API
- * å®žçŽ°å®ƒã€‚
  * @author degloba
  */
 public abstract class NamedQueryParser {
