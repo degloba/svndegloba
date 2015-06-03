e339fc53ab0994a2c1616395d32f485164a4530e
degloba <degloba@degloba.com>
2015-06-03 19:35:54 +0200

    Refactoring

--- degloba-persistence-datanucleus/src/test/java/com/google/appengine/datanucleus/query/JPQLQueryTest.java
+++ degloba-persistence-datanucleus/src/test/java/com/google/appengine/datanucleus/query/JPQLQueryTest.java
@@ -1,18 +1,3 @@
-/**********************************************************************
- Copyright (c) 2009 Google Inc.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- **********************************************************************/
 package com.google.appengine.datanucleus.query;
 
 import com.google.appengine.api.datastore.DatastoreFailureException;
@@ -105,7 +90,7 @@ import javax.persistence.TypedQuery;
 //import static com.google.appengine.datanucleus.test.jdo.Flight.newFlightEntity;
 
 /**
- * @author Max Ross <maxr@google.com>
+ * @author degloba
  */
 public class JPQLQueryTest extends JPATestCase {
 
