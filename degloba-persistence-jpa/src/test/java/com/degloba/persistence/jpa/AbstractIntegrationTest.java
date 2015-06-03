3f02f3205b7db770c9de7998b03f1b005a820258
degloba <degloba@degloba.com>
2015-06-03 19:44:48 +0200

    Suport de "Test amb Spring (IOC)"

--- degloba-persistence-jpa/src/test/java/com/degloba/persistence/jpa/AbstractIntegrationTest.java
+++ degloba-persistence-jpa/src/test/java/com/degloba/persistence/jpa/AbstractIntegrationTest.java
@@ -1,9 +1,14 @@
 package com.degloba.persistence.jpa;
 
-import org.dayatang.btm.BtmUtils;
+//import org.dayatang.btm.BtmUtils;
 
+// Classes de domini
 import com.degloba.domain.AbstractEntity;
+import com.degloba.domain.BaseAggregateRoot;
+import com.degloba.domain.EntityRepository;
 import com.degloba.domain.InstanceFactory;
+import com.degloba.ioc.spring.factory.SpringInstanceProvider;
+// Per testejar appengine
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
@@ -11,24 +16,43 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+
+
+import javax.inject.Inject;
+// JPA
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.transaction.SystemException;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/applicationContext.xml")
+@TransactionConfiguration(transactionManager = "transactionManager")
+//@Transactional
+@EnableTransactionManagement
 public class AbstractIntegrationTest {
 
-    private static BtmUtils btmUtils;
+    //private static BtmUtils btmUtils;
 
-    private static EntityManagerFactory emf;
+    //private static EntityManagerFactory emf;
 
     protected EntityManager entityManager;
 
-    private EntityTransaction tx;
+    //private EntityTransaction tx;
 
-    protected EntityRepositoryJpa repository;
+    protected EntityRepository repository;
+    
+    @Inject
+    private ApplicationContext applicationContext;
     
 	private final LocalServiceTestHelper helper =
 	        new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig()
@@ -38,37 +62,42 @@ public class AbstractIntegrationTest {
     public static void setUpClass() throws Exception {
         //btmUtils = BtmUtils.readConfigurationFromClasspath("/datasources.properties");
         //btmUtils.setupDataSource();
-        emf = Persistence.createEntityManagerFactory("transactions-optional");
+        //emf = Persistence.createEntityManagerFactory("transactions-optional");
     }
 
     @AfterClass
     public static void tearDownClass() throws Exception {
-        emf.close();
+        //emf.close();
         //btmUtils.closeDataSource();
         //btmUtils = null;
-        System.out.println("================================================");
-        System.out.println("Close BTM");
+        //System.out.println("================================================");
+        //System.out.println("Close BTM");
     }
 
     @Before
     public void setUp() {
+        SpringInstanceProvider provider = new SpringInstanceProvider(applicationContext);
+        InstanceFactory.setInstanceProvider(provider);
+        
     	helper.setUp();  
     	
-        InstanceFactory.bind(EntityManagerFactory.class, emf);
+        /*InstanceFactory.bind(EntityManagerFactory.class, emf);
         repository = new EntityRepositoryJpa(emf);
         AbstractEntity.setRepository(repository);
         entityManager = repository.getEntityManager();
         tx = entityManager.getTransaction();
-        tx.begin();
+        tx.begin();*/
     }
 
     @After
     public void tearDown() throws IllegalStateException, SystemException {
-        tx.rollback();
+        /*tx.rollback();
         entityManager.close();
         repository = null;
-        AbstractEntity.setRepository(null);
-        
+        BaseAggregateRoot.setRepository(null);
+        */
+    	InstanceFactory.setInstanceProvider(null);
+    	
         helper.tearDown();
     }
 }
