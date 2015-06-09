5d7172f98c1b937d02bca7c2e89ee402f026d850
degloba <degloba@degloba.com>
2015-06-09 19:39:53 +0200

    Refactoring

--- boundedContext1-domain/src/test/java/com/degloba/domain/AccountabilityTest.java
+++ boundedContext1-domain/src/test/java/com/degloba/domain/AccountabilityTest.java
@@ -3,10 +3,9 @@ package com.degloba.domain;
 import org.dayatang.utils.DateUtils;
 import org.junit.Test;
 
-
-
 import org.springframework.transaction.annotation.Transactional;
 
+// Entitats de domini 
 import com.degloba.organisation.domain.Accountability;
 import com.degloba.organisation.domain.Company;
 import com.degloba.organisation.domain.CompanyDepartment;
@@ -16,7 +15,7 @@ import com.degloba.organisation.domain.Employment;
 import com.degloba.organisation.domain.OrgLineMgmt;
 import com.degloba.organisation.domain.Person;
 import com.degloba.organisation.utils.OrganisationUtils;
-import com.degloba.organisation.domain.demo.*;
+
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -32,39 +31,14 @@ public class AccountabilityTest extends AbstractIntegrationTest {
         OrganisationUtils organisationUtils = new OrganisationUtils();
         Date date = DateUtils.date(2012, 1, 3);
         
-/*        Company company = organisationUtils.createCompany("总公司", date);
-        Department financial = organisationUtils.createDepartment("财务部", company, date);*/
+        Company company = organisationUtils.createCompany("总公司", date);
+        Department financial = organisationUtils.createDepartment("财务部", company, date);
         
         CompanyDepartment companyDepartment = organisationUtils.createCompanyDepartment("总公司", date);
         CompanyDepartment companyDepartment2 = organisationUtils.createCompanyDepartment("财务部",  date);
        new OrgLineMgmt(companyDepartment, companyDepartment2, date).save();
-        
-       
-       // https://code.google.com/p/datanucleus-appengine/source/browse/
-       Concrete1 c1 = new Concrete1();
-       
-       c1.setBase1Str("c1 base");
-       c1.setConcrete1Str("concrete1Str");
-       
-       Concrete3 c3 = new Concrete3();
-       c3.setStr("concrete3");
-       
-       c1.setConcrete3(c3);
-       
-       Concrete4 c4 = new Concrete4();
-       c4.setStr("concrete4");
-       
-       List<Concrete4> lstConcrete4= new ArrayList<Concrete4>();
-       lstConcrete4.add(c4);
-       
-       c1.setConcrete4(lstConcrete4);
-       
-       c1.save();
-       
-       Concrete1 c = c1.getConcrete1(c1);
-       
-       String a = "jjj";
-       /* Person person = organisationUtils.createPerson("Martin", "Fowler");
+          
+        Person person = organisationUtils.createPerson("Martin", "Fowler");
         Employee employee = organisationUtils.createEmployee(person, date);
         Employment employment = new Employment(company, employee, date);
         employment.save();
@@ -72,7 +46,7 @@ public class AccountabilityTest extends AbstractIntegrationTest {
         // 断言找到所有的子类实例。
         OrgLineMgmt lineMgmt = OrgLineMgmt.getByResponsible(companyDepartment2, date);
         assertTrue(results.contains(lineMgmt));
-        assertTrue(results.contains(employment));  */
+        assertTrue(results.contains(employment));  
     }
 
 }
