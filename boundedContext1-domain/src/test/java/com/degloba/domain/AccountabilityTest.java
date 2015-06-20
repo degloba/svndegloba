2f174cc16becc0040d272d3bd5f50769099e2c84
degloba <degloba@degloba.com>
2015-06-20 18:27:35 +0200

    Refactoring

--- boundedContext1-domain/src/test/java/com/degloba/domain/AccountabilityTest.java
+++ boundedContext1-domain/src/test/java/com/degloba/domain/AccountabilityTest.java
@@ -2,8 +2,9 @@ package com.degloba.domain;
 
 import org.dayatang.utils.DateUtils;
 import org.junit.Test;
-
 import org.springframework.transaction.annotation.Transactional;
+
+
 
 // Entitats de domini 
 import com.degloba.organisation.domain.Accountability;
@@ -17,11 +18,13 @@ import com.degloba.organisation.domain.Person;
 import com.degloba.organisation.utils.OrganisationUtils;
 
 
+
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class AccountabilityTest extends AbstractIntegrationTest {
 
@@ -31,11 +34,12 @@ public class AccountabilityTest extends AbstractIntegrationTest {
         OrganisationUtils organisationUtils = new OrganisationUtils();
         Date date = DateUtils.date(2012, 1, 3);
         
-        Company company = organisationUtils.createCompany("总公司", date);
-        Department financial = organisationUtils.createDepartment("财务部", company, date);
+        Company company = organisationUtils.createCompany("Oficina central", date);
+                
+        Department financial = organisationUtils.createDepartment("Departament de finances", company, date);
         
-        CompanyDepartment companyDepartment = organisationUtils.createCompanyDepartment("总公司", date);
-        CompanyDepartment companyDepartment2 = organisationUtils.createCompanyDepartment("财务部",  date);
+        CompanyDepartment companyDepartment = organisationUtils.createCompanyDepartment("Oficina central", date);
+        CompanyDepartment companyDepartment2 = organisationUtils.createCompanyDepartment("Departament de finances",  date);
        new OrgLineMgmt(companyDepartment, companyDepartment2, date).save();
           
         Person person = organisationUtils.createPerson("Martin", "Fowler");
@@ -43,7 +47,7 @@ public class AccountabilityTest extends AbstractIntegrationTest {
         Employment employment = new Employment(company, employee, date);
         employment.save();
         List<Accountability> results = Accountability.findAccountabilities(Accountability.class, date);
-        // 断言找到所有的子类实例。
+        // Troba totes les subclasses.
         OrgLineMgmt lineMgmt = OrgLineMgmt.getByResponsible(companyDepartment2, date);
         assertTrue(results.contains(lineMgmt));
         assertTrue(results.contains(employment));  
