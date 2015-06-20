919fcd704999c37855e68cc20768c278ae55096b
degloba <degloba@degloba.com>
2015-06-20 18:18:52 +0200

    Refactoring

--- boundedContext1-domain/src/test/java/com/degloba/domain/LineMgmtTest.java
+++ boundedContext1-domain/src/test/java/com/degloba/domain/LineMgmtTest.java
@@ -20,8 +20,8 @@ public class LineMgmtTest extends AbstractIntegrationTest {
 	public final void testFindResponsiblesOfOrganization() {
 		OrganisationUtils organisationUtils = new OrganisationUtils();
 		Date date = DateUtils.date(2012, 1, 3);
-		Company company = organisationUtils.createCompany("总公司", date);
-		Department financial = organisationUtils.createDepartment("财务部", date);
+		Company company = organisationUtils.createCompany("Oficina central", date);
+		Department financial = organisationUtils.createDepartment("Departament De Finances", date);
 		//OrgLineMgmt lineMgmt = new OrgLineMgmt(company, financial, date);
 		//lineMgmt.save();
 		System.out.println(OrgLineMgmt.findAll().size());
