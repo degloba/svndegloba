316f5a92f08ebd8d8c439c8dd327fe72d8043486
degloba <degloba@degloba.com>
2015-06-23 20:29:28 +0200

    Ref

--- boundedContext1-facade/src/main/java/com/degloba/organisation/facade/OrganisationFacade.java
+++ boundedContext1-facade/src/main/java/com/degloba/organisation/facade/OrganisationFacade.java
@@ -18,19 +18,21 @@ package com.degloba.organisation.facade;
 
 import java.util.Date;
 
+import com.google.appengine.api.datastore.Key;
+
 /**
  * @author yyang
  */
 public interface OrganisationFacade {
 
 
-    void createOrganization(OrganizationDto orgToCreate, Long parentOrgId, Date date);
+    void createOrganization(OrganizationDto orgToCreate, Key parentOrgId, Date date);
 
-    void terminateParty(Long partyId, Date date);
+    void terminateParty(Key partyId, Date date);
 
-    void changeParentOfOrganization(Long organizationId, Long newParentId, Date date);
+    void changeParentOfOrganization(Key organizationId, Key newParentId, Date date);
 
-    void createPostUnderOrganization(PostDto postDto, Long organizationId, Date date);
+    void createPostUnderOrganization(PostDto postDto, Key organizationId, Date date);
 
-    PostDto getPost(long postId);
+    PostDto getPost(Key postId);
 }
