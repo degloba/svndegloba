7363a35aad166fab201356d4db05d2641d1512aa
degloba <degloba@degloba.com>
2015-06-23 20:30:05 +0200

    Ref

--- boundedContext1-facade-impl/src/main/java/com/degloba/organisation/facade/impl/OrganisationFacadeImpl.java
+++ boundedContext1-facade-impl/src/main/java/com/degloba/organisation/facade/impl/OrganisationFacadeImpl.java
@@ -18,14 +18,18 @@ package com.degloba.organisation.facade.impl;
 import com.degloba.organisation.domain.Party;
 import com.degloba.organisation.domain.Post;
 import com.degloba.organisation.facade.assembler.OrganizationAssembler;
+
 import java.util.Date;
+
 import javax.inject.Inject;
-import com.degloba.organisation.application.OrganisationApplication;
+
+import com.degloba.organisation.api.OrganisationService;
 import com.degloba.organisation.domain.Organization;
 import com.degloba.organisation.facade.OrganisationFacade;
 import com.degloba.organisation.facade.OrganizationDto;
 import com.degloba.organisation.facade.PostDto;
 import com.degloba.organisation.facade.assembler.PostAssembler;
+import com.google.appengine.api.datastore.Key;
 
 /**
  *
@@ -34,41 +38,41 @@ import com.degloba.organisation.facade.assembler.PostAssembler;
 public class OrganisationFacadeImpl implements OrganisationFacade {
 
     @Inject
-    protected OrganisationApplication application;
+    protected OrganisationService application;
 
-    public OrganisationFacadeImpl(OrganisationApplication application) {
+    public OrganisationFacadeImpl(OrganisationService application) {
         this.application = application;
     }
 
     @Override
-    public void createOrganization(OrganizationDto orgToCreate, Long parentOrgId, Date date) {
+    public void createOrganization(OrganizationDto orgToCreate, Key parentOrgId, Date date) {
         Organization organization = new OrganizationAssembler().toEntity(orgToCreate);
         Organization parent = application.getEntity(Organization.class, parentOrgId);
         application.createOrganization(organization, parent, date);
     }
 
     @Override
-    public void terminateParty(Long partyId, Date date) {
+    public void terminateParty(Key partyId, Date date) {
         Party party = application.getEntity(Party.class, partyId);
         application.terminateParty(party, date);
     }
 
     @Override
-    public void changeParentOfOrganization(Long organizationId, Long newParentId, Date date) {
+    public void changeParentOfOrganization(Key organizationId, Key newParentId, Date date) {
         Organization organization = application.getEntity(Organization.class, organizationId);
         Organization parent = application.getEntity(Organization.class, newParentId);
         application.changeParentOfOrganization(organization, parent, date);
     }
 
     @Override
-    public void createPostUnderOrganization(PostDto postDto, Long organizationId, Date date) {
+    public void createPostUnderOrganization(PostDto postDto, Key organizationId, Date date) {
         Post post = new PostAssembler().toEntity(postDto);
         Organization organization = application.getEntity(Organization.class, organizationId);
         application.createPostUnderOrganization(post, organization, date);
     }
 
     @Override
-    public PostDto getPost(long postId) {
+    public PostDto getPost(Key postId) {
         Post post = application.getEntity(Post.class, postId);
         return new PostAssembler().toDto(post);
     }
@@ -78,14 +82,13 @@ public class OrganisationFacadeImpl implements OrganisationFacade {
 		// TODO Auto-generated constructor stub
 	}
 
-	public OrganisationApplication getApplication() {
+	public OrganisationService getApplication() {
 		return application;
 	}
 
-	public void setApplication(OrganisationApplication application) {
+	public void setApplication(OrganisationService application) {
 		this.application = application;
 	}
-	
-	
 
+	
 }
