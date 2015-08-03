1090c9d9688468ba9204051512d5390db9fb9192
degloba <degloba@degloba.com>
2015-08-03 19:55:16 +0200

    Ref

--- boundedContext1-facade/src/main/java/com/degloba/organisation/facade/OrganisationFacade.java
+++ boundedContext1-facade/src/main/java/com/degloba/organisation/facade/OrganisationFacade.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2014 Dayatang Open Source..
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.degloba.organisation.facade;
 
 import java.util.Date;
@@ -21,10 +5,9 @@ import java.util.Date;
 import com.google.appengine.api.datastore.Key;
 
 /**
- * @author yyang
+ * @author degloba
  */
 public interface OrganisationFacade {
-
 
     void createOrganization(OrganizationDto orgToCreate, Key parentOrgId, Date date);
 
@@ -35,4 +18,5 @@ public interface OrganisationFacade {
     void createPostUnderOrganization(PostDto postDto, Key organizationId, Date date);
 
     PostDto getPost(Key postId);
+
 }
