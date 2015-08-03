06cf9046a0988cb68604f91df10d50915fd06126
degloba <degloba@degloba.com>
2015-08-03 19:55:53 +0200

    Ref

--- boundedContext1-facade-impl/src/main/java/com/degloba/organisation/facade/assembler/PostAssembler.java
+++ boundedContext1-facade-impl/src/main/java/com/degloba/organisation/facade/assembler/PostAssembler.java
@@ -3,9 +3,7 @@ package com.degloba.organisation.facade.assembler;
 import com.degloba.organisation.domain.Post;
 import com.degloba.organisation.facade.PostDto;
 
-/**
- * Created by yyang on 14-8-12.
- */
+
 public class PostAssembler {
     public Post toEntity(PostDto postDto) {
         return null;
