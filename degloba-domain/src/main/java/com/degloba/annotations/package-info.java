b3993e9a9c2cc5ab581121de827257a958e3b1c5
degloba <degloba@degloba.com>
2015-08-03 19:49:35 +0200

    Ref

--- degloba-domain/src/main/java/com/degloba/annotations/package-info.java
+++ degloba-domain/src/main/java/com/degloba/annotations/package-info.java
@@ -1,5 +1,1 @@
-/**
- * 领域层Annotation。
- * Created by yyang on 14-2-5.
- */
 package com.degloba.annotations;
--- degloba-domain/src/main/java/com/degloba/domain/package-info.java
+++ degloba-domain/src/main/java/com/degloba/domain/package-info.java
@@ -1,5 +1,1 @@
-/**
- * 领域层接口、基类和实现类。
- * Created by yyang on 14-2-5.
- */
 package com.degloba.domain;
