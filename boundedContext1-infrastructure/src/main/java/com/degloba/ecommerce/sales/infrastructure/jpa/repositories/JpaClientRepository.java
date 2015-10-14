66d7239359364d366468d013cb2ee456be9efc49
pere <pere@192.168.1.119>
2015-10-14 18:23:49 +0200

    r

---
+++ boundedContext1-infrastructure/src/main/java/com/degloba/ecommerce/sales/infrastructure/jpa/repositories/JpaClientRepository.java
@@ -0,0 +1,32 @@
+package com.degloba.ecommerce.sales.infrastructure.jpa.repositories;
+
+import com.degloba.annotations.DomainRepositoryImpl;
+import com.degloba.domain.EntityRepositoryJpa;
+import com.degloba.domain.GenericJpaRepository;
+import com.degloba.ecommerce.crm.domain.Customer;
+import com.degloba.ecommerce.crm.domain.ICustomerRepository;
+import com.degloba.organisation.domain.client.Client;
+import com.degloba.organisation.domain.client.IClientRepository;
+import com.google.appengine.api.datastore.Key;
+
+/**
+ * @author degloba
+ *
+ */
+@DomainRepositoryImpl
+//public class JpaCustomerRepository extends GenericJpaRepository<Customer> implements CustomerRepository{
+public class JpaClientRepository extends EntityRepositoryJpa implements IClientRepository{
+
+	@Override
+	public Client load(Key id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Client entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
