85f15a28a14b571aeb121d6470bcc443b48903b5
pere <pere@192.168.1.119>
2015-10-14 19:02:21 +0200

    r

---
+++ boundedContext1-infrastructure/src/main/java/com/degloba/ecommerce/sales/infrastructure/jpa/repositories/JpaProductRepository.java
@@ -0,0 +1,38 @@
+package com.degloba.ecommerce.sales.infrastructure.jpa.repositories;
+
+import java.util.List;
+
+import com.degloba.annotations.DomainRepositoryImpl;
+import com.degloba.domain.EntityRepositoryJpa;
+import com.degloba.domain.GenericJpaRepository;
+import com.degloba.ecommerce.sales.productscatalog.IProductRepository;
+import com.degloba.ecommerce.sales.productscatalog.Product;
+import com.google.appengine.api.datastore.Key;
+
+/**
+ * @author degloba
+ *
+ */
+@DomainRepositoryImpl
+//public class JpaCustomerRepository extends GenericJpaRepository<Customer> implements CustomerRepository{
+public class JpaProductRepository extends EntityRepositoryJpa implements IProductRepository{
+
+	@Override
+	public Product load(Key id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> findProductWhereBestBeforeExpiredIn(int days) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+/*	@Override
+	public void save(Product entity) {
+		// TODO Auto-generated method stub
+		
+	}*/
+
+}
---
+++ boundedContext1-infrastructure/src/main/java/com/degloba/ecommerce/sales/infrastructure/jpa/repositories/JpaReservationRepository.java
@@ -0,0 +1,32 @@
+package com.degloba.ecommerce.sales.infrastructure.jpa.repositories;
+
+import java.util.List;
+
+import com.degloba.annotations.DomainRepositoryImpl;
+import com.degloba.domain.EntityRepositoryJpa;
+import com.degloba.domain.GenericJpaRepository;
+import com.degloba.ecommerce.sales.reservation.IReservationRepository;
+import com.degloba.ecommerce.sales.reservation.Reservation;
+import com.google.appengine.api.datastore.Key;
+
+/**
+ * @author degloba
+ *
+ */
+@DomainRepositoryImpl
+//public class JpaCustomerRepository extends GenericJpaRepository<Customer> implements CustomerRepository{
+public class JpaReservationRepository extends EntityRepositoryJpa implements IReservationRepository{
+
+	@Override
+	public Reservation load(Key id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Reservation reservation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
