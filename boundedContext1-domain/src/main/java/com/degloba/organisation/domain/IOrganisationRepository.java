f30086f594cf73b8dbd2b2a00c7088db7189490c
pere <pere@192.168.1.119>
2015-10-14 17:49:46 +0200

    r

---
+++ boundedContext1-domain/src/main/java/com/degloba/organisation/domain/IOrganisationRepository.java
@@ -0,0 +1,18 @@
+package com.degloba.organisation.domain;
+
+import com.google.appengine.api.datastore.Key;
+
+import com.degloba.annotations.DomainRepository;
+//import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.AggregateId;
+
+/**
+ * @author degloba
+ * 
+ */
+@DomainRepository
+public interface IOrganisationRepository {
+
+	public Organization load(Key id);
+
+	public void save(Organization entity);
+}
