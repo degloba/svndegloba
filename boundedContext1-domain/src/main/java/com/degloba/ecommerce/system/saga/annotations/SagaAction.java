2b297c09c74462a400db1fa11ab17770b8412e15
pere <pere@192.168.1.119>
2015-10-27 20:29:07 +0100

    r

--- boundedContext1-domain/src/main/java/com/degloba/ecommerce/system/saga/annotations/SagaAction.java
+++
@@ -1,9 +0,0 @@
-package com.degloba.ecommerce.system.saga.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface SagaAction {
-
-}
