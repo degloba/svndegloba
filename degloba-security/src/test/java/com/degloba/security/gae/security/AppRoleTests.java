c4213ed4d01941b5a5a5c28048dd61bc6b2434bd
degloba Projects <degloba@degloba.com>
2015-11-19 19:57:42 +0100

    r

---
+++ degloba-security/src/test/java/com/degloba/security/gae/security/AppRoleTests.java
@@ -0,0 +1,30 @@
+package com.degloba.security.gae.security;
+
+import static org.junit.Assert.*;
+
+import com.degloba.security.gae.security.AppRole;
+
+import org.junit.Test;
+import org.springframework.security.core.GrantedAuthority;
+
+/**
+ * @author Luke Taylor
+ */
+public class AppRoleTests {
+
+	@Test
+	public void getAuthorityReturnsRoleName() {
+		GrantedAuthority admin = AppRole.ADMIN;
+
+		assertEquals("ROLE_ADMIN", admin.getAuthority());
+	}
+
+	@Test
+	public void bitsAreCorrect() throws Exception {
+		// If this fails, someone has modified the Enum and the Datastore is probably
+		// corrupt!
+		assertEquals(0, AppRole.ADMIN.getBit());
+		assertEquals(1, AppRole.NEW_USER.getBit());
+		assertEquals(2, AppRole.USER.getBit());
+	}
+}
