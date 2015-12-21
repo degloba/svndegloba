e22e8951fc11872f6d00b5a2378a3d269e0d0a67
degloba Projects <degloba@degloba.com>
2015-12-21 19:36:47 +0100

    R

--- boundedContext1-infrastructure/src/main/java/com/degloba/ecommerce/sales/infrastructure/jpa/repositories/JpaClientRepository.java
+++ boundedContext1-infrastructure/src/main/java/com/degloba/ecommerce/sales/infrastructure/jpa/repositories/JpaClientRepository.java
@@ -2,11 +2,13 @@ package com.degloba.ecommerce.sales.infrastructure.jpa.repositories;
 
 // Domain
 import com.degloba.domain.annotations.DomainRepositoryImpl;
+import com.degloba.ecommerce.sales.client.domain.Client;
+import com.degloba.ecommerce.sales.client.domain.IClientRepository;
 import com.degloba.domain.JpaEntityRepository;
 
 // Domain (organisation)
-import com.degloba.organisation.domain.client.Client;
-import com.degloba.organisation.domain.client.IClientRepository;
+/*import com.degloba.organisation.domain.client.Client;
+import com.degloba.organisation.domain.client.IClientRepository;*/
 import com.google.appengine.api.datastore.Key;
 
 /**
