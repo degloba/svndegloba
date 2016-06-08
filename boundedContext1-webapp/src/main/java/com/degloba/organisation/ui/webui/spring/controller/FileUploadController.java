58de88426dc0faa6fa31f2c1ded7fa1cb9120c24
degloba Projects <degloba@degloba.com>
2016-06-08 19:51:54 +0200

    O

---
+++ boundedContext1-webapp/src/main/java/com/degloba/organisation/ui/webui/spring/controller/FileUploadController.java
@@ -0,0 +1,38 @@
+package com.degloba.organisation.ui.webui.spring.controller;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileUploadController {
+	
+	private UploadedFile uploadedFile;
+
+    public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    
+    
+    
+    public UploadedFile getUploadedFile() {
+        return uploadedFile;
+    }
+
+    public void setUploadedFile(UploadedFile uploadedFile) {
+        this.uploadedFile = uploadedFile;
+    }
+
+    
+    public void submit() {    
+        // Get information you from the uploaded file
+        System.out.println("Uploaded file name : " + uploadedFile.getFileName());
+    }
+
+}
