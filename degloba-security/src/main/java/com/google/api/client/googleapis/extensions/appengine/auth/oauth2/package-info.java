24dec7c71ab809f202695badb053f189bedd97b2
degloba Projects <degloba@degloba.com>
2016-07-07 20:10:51 +0200

    O

--- degloba-security/src/main/java/com/google/api/client/googleapis/extensions/appengine/auth/oauth2/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Google App Engine utilities for OAuth 2.0 for Google APIs.
- *
- * @since 1.7
- * @author Yaniv Inbar
- */
-
-package com.google.api.client.googleapis.extensions.appengine.auth.oauth2;
-
