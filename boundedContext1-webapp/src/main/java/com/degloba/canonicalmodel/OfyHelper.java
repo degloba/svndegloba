801ad1d2959b2f6409cd365de76437a1dc78d634
degloba Projects <degloba@degloba.com>
2016-07-14 20:00:45 +0200

    o

---
+++ boundedContext1-webapp/src/main/java/com/degloba/canonicalmodel/OfyHelper.java
@@ -0,0 +1,34 @@
+package com.degloba.canonicalmodel;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import com.degloba.rent.domain.objectify.Category;
+import com.degloba.rent.domain.objectify.Location;
+import com.degloba.rent.domain.objectify.Owner;
+import com.degloba.rent.domain.objectify.Photo;
+import com.degloba.rent.domain.objectify.Product;
+import com.degloba.rent.domain.objectify.Subcategory;
+import com.googlecode.objectify.ObjectifyService;
+
+public class OfyHelper implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// App Engine does not currently invoke this method.
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// This will be invoked as part of a warmup request, or the first user request if no warmup
+	    // request.
+	    ObjectifyService.register(Category.class);
+	    ObjectifyService.register(Subcategory.class);
+	    ObjectifyService.register(Photo.class);
+	    ObjectifyService.register(Location.class);
+	    ObjectifyService.register(Product.class);
+	    ObjectifyService.register(Owner.class);
+
+	}
+
+}
