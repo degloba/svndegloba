512d2a1fce661a5f81932aa21c3dcc0096d02e3e
degloba Projects <degloba@degloba.com>
2016-07-15 20:58:55 +0200

    o

--- boundedContext1-webapp/src/main/java/com/degloba/rent/ui/webui/spring/controller/FileUploadController.java
+++ boundedContext1-webapp/src/main/java/com/degloba/rent/ui/webui/spring/controller/FileUploadController.java
@@ -13,15 +13,17 @@ import org.primefaces.event.FileUploadEvent;
 import org.springframework.stereotype.Component;
 
 import com.degloba.gcs.StorageUtils;
+
+import com.degloba.objectify.GenericDao;
 import com.degloba.rent.cqrs.readmodel.CategoryFinder;
-import com.degloba.rent.domain.jpa.Category;
+
 import com.degloba.rent.domain.jpa.Owner;
 import com.degloba.rent.domain.jpa.Photo;
 import com.degloba.rent.domain.jpa.Product;
-import com.degloba.rent.domain.jpa.Subcategory;
-import com.degloba.rent.facade.CategoryFacade;
-import com.degloba.rent.facade.PhotoFacade;
-import com.degloba.rent.facade.SubcategoryFacade;
+
+import com.degloba.rent.facade.jpa.CategoryFacade;
+import com.degloba.rent.facade.jpa.PhotoFacade;
+import com.degloba.rent.facade.jpa.SubcategoryFacade;
 
 
 @Component
@@ -32,16 +34,10 @@ public class FileUploadController {
 		
     @Inject
     protected PhotoFacade facadePhoto;
+
     
     @Inject
-    protected SubcategoryFacade facadeSubcategory;
-    
-    @Inject
-    protected CategoryFacade facadeCategory;
-    
-    
-    @Inject
-    protected CategoryFinder finderCategory;
+    protected GenericDao categoryRepositoryObjectify;
     
     
     public void handleFileUpload(FileUploadEvent event) 
@@ -52,58 +48,18 @@ public class FileUploadController {
 			try {
 					// Insertem a GCS
 					StorageUtils.uploadFile(file, event.getFile().getContentType(), event.getFile().getInputstream(),event.getFile().getSize(), "wwwdegloba-1350.appspot.com");
-			    	
-	    		    
-	    		    // Persistim Photo
-	    		    Photo photo = new Photo();
-	    		    	    		    
-	    		    Owner owner = new Owner();
-	    		    Product product = new Product();
-	    		    
-	    		    //////photo.setCategory(category);
-	    		    photo.setProduct(product);
-	    		    
-	    		    photo.setIdGcs(file);
-	    		   /////////// facadePhoto.createPhoto(photo);
-	    		    
-	    		    
-	    		    // Categories/Subcategories
-	    		    Category category = new Category();
-	    		    category.setDescription("Motor");
-	    		    
-	    		    Subcategory subcategory = new Subcategory();	    		    	    		           		   
-	    		    subcategory.setDescription("Motos");
-	    		    subcategory.setCategory(category);
-	    		    
-	    		    category.getSubcategories().add(subcategory);
-	    		    
-	    		    Subcategory subcategory2 = new Subcategory();	    		    	    		           		   
-	    		    subcategory2.setDescription("Coches");
-	    		    subcategory2.setCategory(category);
-	    		    
-	    		    category.getSubcategories().add(subcategory2);
-	    		    
-	    		    facadeCategory.createCategory(category);
-	    		    
-	    		    // Segon
-	    		    category = new Category();
-	    		    category.setDescription("Deportes");
-	    		    	    		    
-	    		    subcategory = new Subcategory();	    		    	    		           		   
-	    		    subcategory.setDescription("Vela");
-	    		    subcategory.setCategory(category);
-	    		    
-	    		    category.getSubcategories().add(subcategory);
-	    		    
-	    		    subcategory2 = new Subcategory();	    		    	    		           		   
-	    		    subcategory2.setDescription("Futbol");
-	    		    subcategory2.setCategory(category);
-	    		    category.getSubcategories().add(subcategory2);
-	    		    
-	    		    facadeCategory.createCategory(category);
-	    		    
-	    		    
-	    		    String a = "1";
+					
+				    // Persistim Photo
+				    Photo photo = new Photo();
+				    	    		    
+				    Owner owner = new Owner();
+				    Product product = new Product();
+				    
+				    //////photo.setCategory(category);
+				    photo.setProduct(product);
+				    
+				    photo.setIdGcs(file);
+				   /////////// facadePhoto.createPhoto(photo);
 				} catch (GeneralSecurityException e) {
 					// TODO Auto-generated catch block
 					
