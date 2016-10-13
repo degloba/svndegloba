c8325b26062ed44938b3078dd65a8a2eb9f5331b
degloba Projects <degloba@degloba.com>
2016-10-13 19:41:09 +0200

    O

--- degloba-domain/src/main/java/com/degloba/domain/persistence/nosql/googleDatastore/api/jpa/internal/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Achieve internal domain layer need to use the class. 
- * User code should never inherit or use this package inside the class.
- */
-package com.degloba.domain.persistence.nosql.googleDatastore.api.jpa.internal;
