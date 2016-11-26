a1b3caf5cea042f29dde1d7830b58344d106931e
degloba Projects <degloba@degloba.com>
2016-11-26 20:05:02 +0100

    Funcionant

--- boundedContext1-domain/src/test/java/com/degloba/domain/CategoriesTest.java
+++ boundedContext1-domain/src/test/java/com/degloba/domain/CategoriesTest.java
@@ -4,6 +4,9 @@ package com.degloba.domain;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.degloba.domain.ioc.InstanceFactory;
+import com.degloba.domain.persistence.rdbms.jpa.IEntityRepository;
+import com.degloba.domain.persistence.rdbms.jpa.canonicalmodel.publishedlanguage.AggregateId;
 import com.degloba.rent.domain.persistence.nosql.googleDatastore.api.objectify.Subcategory;
 import com.degloba.rent.domain.persistence.rdbms.jpa.CategoryJpa;
 import com.degloba.rent.domain.persistence.rdbms.jpa.IRentRepository;
@@ -22,14 +25,18 @@ public class CategoriesTest extends AbstractIntegrationTest {
     public void setUp() throws Exception {
         super.setUp();
         
+        
         CategoryJpa category = new CategoryJpa();
+        category.setAggregateId(AggregateId.generate());
+        
 	    category.setDescription("Oci");
 	    
         // Categories/Subcategories
 	    subcategory = new SubcategoryJpa();
 	    subcategory.setDescription("Natacion");
+	    subcategory.setAggregateId(AggregateId.generate());
 	    
-	    ///////subcategory.setCategory(category);
+	    subcategory.setCategory(category);
 	    category.getSubcategories().add(subcategory);
 	    
 	    category.save();
@@ -38,12 +45,12 @@ public class CategoriesTest extends AbstractIntegrationTest {
     
     @Test
     public void testSubcategories() {
-        String jpql = "select o from Category o";
-        IRentRepository repository = InstanceFactory.getInstance(IRentRepository.class);
-        List<CategoryJpa> categories = repository.createJpqlQuery(jpql).list();
+        String jpql = "select o from CategoryJpa o";
+        IEntityRepository rentRepository = InstanceFactory.getInstance(IEntityRepository.class);
+        List<CategoryJpa> categories = rentRepository.createJpqlQuery(jpql).list();
         
-        String jpql2 = "select o from Subcategory o";
-        List<Subcategory> subcategories = repository.createJpqlQuery(jpql2).list();
+        String jpql2 = "select o from SubcategoryJpa o";
+        List<SubcategoryJpa> subcategories = rentRepository.createJpqlQuery(jpql2).list();
                 
  //       assertFalse(persons.contains(person1));
         assertEquals(subcategories.get(0).getDescription(),subcategory.getDescription());
