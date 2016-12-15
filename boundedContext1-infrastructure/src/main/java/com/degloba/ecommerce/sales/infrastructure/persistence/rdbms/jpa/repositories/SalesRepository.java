9ff5cfea791d8236553ea01da47bdce27f15b6a7
degloba Projects <degloba@degloba.com>
2016-12-15 18:58:34 +0100

    o

--- boundedContext1-infrastructure/src/main/java/com/degloba/ecommerce/sales/infrastructure/persistence/rdbms/jpa/repositories/SalesRepository.java
+++ boundedContext1-infrastructure/src/main/java/com/degloba/ecommerce/sales/infrastructure/persistence/rdbms/jpa/repositories/SalesRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 // Domain
 import com.degloba.domain.annotations.DomainRepositoryImpl;
+import com.degloba.domain.persistence.rdbms.jpa.BaseAggregateRoot;
 import com.degloba.domain.persistence.rdbms.jpa.EntityRepository;
 import com.degloba.domain.persistence.rdbms.jpa.canonicalmodel.publishedlanguage.AggregateId;
 import com.degloba.ecommerce.sales.domain.persistence.rdbms.jpa.ISalesRepository;
 import com.degloba.ecommerce.sales.productscatalog.domain.persistence.rdbms.jpa.Product;
+import com.degloba.ecommerce.sales.reservation.domain.persistence.rdbms.jpa.Reservation;
 
 
 /**
@@ -24,7 +26,21 @@ public class SalesRepository extends EntityRepository implements ISalesRepositor
 	}
 
 	@Override
-	public Product load(Class<Product> class1, AggregateId productId) {
+	public Product loadProduct(Class<Product> class1, AggregateId productId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	@Override
+	public <T extends BaseAggregateRoot> T save(T entitat) {
+		return entitat;
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Reservation loadReservation(Class<Reservation> clazz, AggregateId orderId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
