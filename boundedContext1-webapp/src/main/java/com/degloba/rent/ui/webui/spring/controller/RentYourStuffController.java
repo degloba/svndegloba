1a65ac575ecc647c054594e467226c8f0612a00c
degloba Projects <degloba@degloba.com>
2016-12-16 20:52:54 +0100

    o

--- boundedContext1-webapp/src/main/java/com/degloba/rent/ui/webui/spring/controller/RentYourStuffController.java
+++ boundedContext1-webapp/src/main/java/com/degloba/rent/ui/webui/spring/controller/RentYourStuffController.java
@@ -21,9 +21,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 // Spring Web
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/*import com.degloba.identityToolkit.GitkitClient;
-import com.degloba.identityToolkit.GitkitClientException;
-import com.degloba.identityToolkit.GitkitUser;*/
+
 //import com.degloba.usuaris.domain.IGCMTokenRegisterRepository;
 //import com.degloba.usuaris.infrastructure.jpa.repositories.GCMTokenRegisterRepository;
 import com.google.appengine.api.utils.SystemProperty;
@@ -57,50 +55,7 @@ public class RentYourStuffController {
 			
 		InputStream keyStream = new FileInputStream(context.getRealPath("/WEB-INF/wwwdegloba-e308deb6a13f.p12"));
 		
-/*		GitkitClient gitkitClient = GitkitClient
-				.newBuilder()
-				.setGoogleClientId(googleClientId)					
-				.setServiceAccountEmail(serviceAccountEmail)
-				.setKeyStream(keyStream)
-				.setWidgetUrl("http://www.degloba.com/gitkit")
-				.setCookieName("gtoken")
-				.setServerApiKey("AIzaSyCnKOhcCFk24l148XUy34lwCLchldbKEYU")
-				.build();
-    	
-    	
-		GitkitUser gitkitUser = null;*/
             
-	    // Això no funciona en GAE!!!
-	    //GitkitClient gitkitClient = GitkitClient.createFromJson("gitkit-server-config.json");
-	  	
-	    /*gitkitUser = gitkitClient.validateToken(gtoken);
-	        
-	    String userInfo = null;
-	    if (gitkitUser != null) 
-	    {	
-	       userInfo = "Welcome back!<br><br> Email: " + gitkitUser.getEmail() + "<br> Id: "
-	              + gitkitUser.getLocalId() + "<br> Provider: " + gitkitUser.getCurrentProvider();
-	         
-	       	logger.info("************** LOGINAT !!!! " + userInfo);
-		}
-	     else 
-	     {
-	    	 logger.info("************** NO LOGINAT!!!");
-	    	 
-	    	 return "home";
-	     }*/
-	        	
-	    
-	   /* HttpSession session = request.getSession();
-	    
-	    if (session.getAttribute("sessionUserKey") == null)
-	    {
-	    	logger.info("************* LoginController : guardem user_Id a la sessio");
-	    			
-	    	session.setAttribute("SessionUserKey", gitkitUser.getLocalId());
-	    }*/
-	    
-	    	
 	    
 	    // *****************************************************************************************  
     	// PERSISTIM (https://developers.google.com/identity/toolkit/web/required-endpoints#overview)
