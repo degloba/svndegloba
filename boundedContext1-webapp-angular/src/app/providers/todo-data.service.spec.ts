27e862a69b701bdd4482571b41ed9136674c1500
degloba Projects <degloba@degloba.com>
2019-03-16 20:45:03 +0100

    rev

---
+++ boundedContext1-webapp-angular/src/app/providers/todo-data.service.spec.ts
@@ -0,0 +1,13 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TodoDataService } from './todo-data.service';
+import { describe } from "jasmine";
+
+describe('TodoDataService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: TodoDataService = TestBed.get(TodoDataService);
+    expect(service).toBeTruthy();
+  });
+});
