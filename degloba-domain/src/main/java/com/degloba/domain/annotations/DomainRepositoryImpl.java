f17192594366d2e4dab9097314472de4d3de7a8f
degloba Projects <degloba@degloba.com>
2019-06-01 21:09:34 +0200

    rev

--- degloba-domain/src/main/java/com/degloba/domain/annotations/DomainRepositoryImpl.java
+++ degloba-domain/src/main/java/com/degloba/domain/annotations/DomainRepositoryImpl.java
@@ -12,9 +12,12 @@ import org.springframework.stereotype.Component;
 
 /**
  * @author degloba
- * @category Defineix el patro Repository
+ * 
+ * @category Defineix el patró Repository
+ * 
+ * @see <a href="https://barradevblog.wordpress.com/2013/04/23/el-patron-repositorio-repository-pattern-implementacion-practica-con-entity-framework/">Patró repositori</a>
+ * 
  */
-
 @Component
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
