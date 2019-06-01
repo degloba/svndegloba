d6cb04b9d92c82d443de5b454991479f3ea1ffec
degloba Projects <degloba@degloba.com>
2019-06-01 21:11:49 +0200

    R

--- degloba-domain/src/main/java/com/degloba/domain/annotations/FinderImpl.java
+++ degloba-domain/src/main/java/com/degloba/domain/annotations/FinderImpl.java
@@ -8,6 +8,14 @@ import java.lang.annotation.Target;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * 
+ * @author degloba
+ * 
+ * @category En el patró CQRS es creen diferents models per a comandes i per a consultes, de manera que es crea un model independent
+ * per cada un d'ells
+ *
+ */
 @Component
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
