fd1dd4afe7a182a43f9e77a1cef5374740ac1e60
degloba Projects <degloba@degloba.com>
2019-06-10 20:18:29 +0200

    r

--- boundedContext1-webapp-angular/documentation/js/menu-wc.js
+++ boundedContext1-webapp-angular/documentation/js/menu-wc.js
@@ -204,6 +204,24 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                         </ul>
                                     </li>
                             </li>
+                            <li class="link">
+                                <a href="modules/ModulModule.html" data-type="entity-link">ModulModule</a>
+                                    <li class="chapter inner">
+                                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                            'data-target="#components-links-module-ModulModule-33652929491fe034b58b78ac9ee795cf"' : 'data-target="#xs-components-links-module-ModulModule-33652929491fe034b58b78ac9ee795cf"' }>
+                                            <span class="icon ion-md-cog"></span>
+                                            <span>Components</span>
+                                            <span class="icon ion-ios-arrow-down"></span>
+                                        </div>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-ModulModule-33652929491fe034b58b78ac9ee795cf"' :
+                                            'id="xs-components-links-module-ModulModule-33652929491fe034b58b78ac9ee795cf"' }>
+                                            <li class="link">
+                                                <a href="components/MiComponenteComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">MiComponenteComponent</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                            </li>
                 </ul>
                 </li>
                     <li class="chapter">
