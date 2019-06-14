01ce20cd09a43be05243689e374b80e74bd3386c
degloba Projects <degloba@degloba.com>
2019-06-14 17:02:19 +0200

    r

--- boundedContext1-webapp-angular/src/environments/environment.prod.ts
+++ boundedContext1-webapp-angular/src/environments/environment.prod.ts
@@ -13,10 +13,10 @@ export const environment = {
   //    "${ENDPOINTS_HOST}/echo?key=${ENDPOINTS_KEY}"
   
   // ENDPOINTS_HOST = https://wwwdegloba-1350.appspot.com
-  // ENDPOINTS_KEY = AIzaSyDJFvo6rPvVdWY_uSw3PYF7IRiRJ5_LaF8
+  // ENDPOINTS_KEY = AIzaSyDJFvo6rPvVdWY_uSw3P.........................
   
   
-  // curl --request POST --header "content-type:application/json" --data '{"message":"hello world"}' "https://wwwdegloba-1350.appspot.com/echo?key=AIzaSyDJFvo6rPvVdWY_uSw3PYF7IRiRJ5_LaF8"
+  // curl --request POST --header "content-type:application/json" --data '{"message":"hello world"}' "https://wwwdegloba-1350.appspot.com/echo?key=AIzaSyDJFvo6rPvVdWY_uSw3P...........
    
   firebase: {
         apiKey : 'AIzaSyDuTo74D7zmb6vO6bOV5Z1h7IABIEBM0hM',
