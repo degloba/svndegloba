1c2e6240231bf88f6a2b4659893a12deba155729
degloba Projects <degloba@degloba.com>
2019-12-06 17:28:28 +0100

    r

---
+++ boundedContext1-db/src/main/java/com/degloba/DbSpringBootApplication.java
@@ -0,0 +1,26 @@
+package com.degloba;
+
+import org.h2.tools.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class DbSpringBootApplication {
+
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers",
+                "-tcpPort", "9090");
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbSpringBootApplication.class, args);
+    }
+}
+
