79957d35d6c0fafeae9ab67a5980cd13adcc72e7
degloba Projects <degloba@degloba.com>
2020-05-05 19:01:52 +0200

    R

---
+++ degloba-domain/src/main/java/com/degloba/domain/specifications/SearchCriteria.java
@@ -0,0 +1,50 @@
+package com.degloba.domain.specifications;
+
+/**
+ * https://github.com/eugenp/tutorials/blob/master/spring-rest-query-language/pom.xml
+ *
+ * @author degloba
+ *
+ */
+public class SearchCriteria {
+
+    private String key;
+    private String operation;
+    private Object value;
+
+    public SearchCriteria() {
+
+    }
+
+    public SearchCriteria(final String key, final String operation, final Object value) {
+        super();
+        this.key = key;
+        this.operation = operation;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(final String key) {
+        this.key = key;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(final String operation) {
+        this.operation = operation;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(final Object value) {
+        this.value = value;
+    }
+
+}
