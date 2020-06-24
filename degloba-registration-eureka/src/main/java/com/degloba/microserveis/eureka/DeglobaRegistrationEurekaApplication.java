59f25bebd2c67ab78bffb2a3c46e78b454027b1a
degloba Projects <degloba@degloba.com>
2020-06-24 19:21:21 +0200

    R

---
+++ degloba-registration-eureka/src/main/java/com/degloba/microserveis/eureka/DeglobaRegistrationEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.degloba.microserveis.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DeglobaRegistrationEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeglobaRegistrationEurekaApplication.class, args);
+	}
+
+}
