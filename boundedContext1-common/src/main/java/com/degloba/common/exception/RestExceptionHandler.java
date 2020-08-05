78a5ae698e489bc79c721207979d4f7208cb7256
degloba <degloba@degloba.com>
2020-08-05 11:48:06 +0000

    sss

--- boundedContext1-common/src/main/java/com/degloba/common/exception/RestExceptionHandler.java
+++ boundedContext1-common/src/main/java/com/degloba/common/exception/RestExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * 
- * @author deglobassss
+ * @author deglobassss1111
  * 
  * @category gestior d'excepcions i estats de resposta http dels controladors d'Spring MVC
  *
